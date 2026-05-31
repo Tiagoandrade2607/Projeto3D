a7d7c438982be49056585db1b9ad9ef3a554e338
Tiagoandrade2607 <tiagoandrade.aluno@unipampa.edu.br>
2026-05-31 01:27:15 -0300

    Add files via upload

--- lesson_reference/player_3d_template.gd
+++ lesson_reference/player_3d_template.gd
@@ -1,18 +1,15 @@
 extends CharacterBody3D
 
 @export_group("Movement")
-## Character maximum run speed on the ground in meters per second.
+
 @export var move_speed := 8.0
-## Ground movement acceleration in meters per second squared.
+
 @export var acceleration := 20.0
-## When the player is on the ground and presses the jump button, the vertical
-## velocity is set to this value.
+
 @export var jump_impulse := 12.0
-## Player model rotation speed in arbitrary units. Controls how fast the
-## character skin orients to the movement or camera direction.
+
 @export var rotation_speed := 12.0
-## Minimum horizontal speed on the ground. This controls when the character skin's
-## animation tree changes between the idle and running states.
+
 @export var stopping_speed := 1.0
 
 @export_group("Camera")
@@ -20,18 +17,15 @@ extends CharacterBody3D
 @export var tilt_upper_limit := PI / 3.0
 @export var tilt_lower_limit := -PI / 8.0
 
-## Each frame, we find the height of the ground below the player and store it here.
-## The camera uses this to keep a fixed height while the player jumps, for example.
 var ground_height := 0.0
 
 var _gravity := -30.0
 var _was_on_floor_last_frame := true
 var _camera_input_direction := Vector2.ZERO
 
-## The last movement or aim direction input by the player. We use this to orient
-## the character model.
+
 @onready var _last_input_direction := global_basis.z
-# We store the initial position of the player to reset to it when the player falls off the map.
+
 @onready var _start_position := global_position
 
 @onready var _camera_pivot: Node3D = %CameraPivot
@@ -79,25 +73,21 @@ func _physics_process(delta: float) -> void:
 
 	_camera_input_direction = Vector2.ZERO
 
-	# Calculate movement input and align it to the camera's direction.
 	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down", 0.4)
-	# Should be projected onto the ground plane.
+
 	var forward := _camera.global_basis.z
 	var right := _camera.global_basis.x
 	var move_direction := forward * raw_input.y + right * raw_input.x
 	move_direction.y = 0.0
 	move_direction = move_direction.normalized()
 
-	# To not orient the character too abruptly, we filter movement inputs we
-	# consider when turning the skin. This also ensures we have a normalized
-	# direction for the rotation basis.
+
 	if move_direction.length() > 0.2:
 		_last_input_direction = move_direction.normalized()
 	var target_angle := Vector3.BACK.signed_angle_to(_last_input_direction, Vector3.UP)
 	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
 
-	# We separate out the y velocity to only interpolate the velocity in the
-	# ground plane, and not affect the gravity.
+
 	var y_velocity := velocity.y
 	velocity.y = 0.0
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
@@ -105,7 +95,6 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector3.ZERO
 	velocity.y = y_velocity + _gravity * delta
 
-	# Character animations and visual effects.
 	var ground_speed := Vector2(velocity.x, velocity.z).length()
 	var is_just_jumping := Input.is_action_just_pressed("jump") and is_on_floor()
 	if is_just_jumping:
